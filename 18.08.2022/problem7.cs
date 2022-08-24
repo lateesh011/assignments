d47d5fad3e856149a44d35b0693bd9a28ddb5cd7
lateesh011 <111356537+lateesh011@users.noreply.github.com>
2022-08-24 08:34:08 +0530

    Create problem7.cs

---
+++ 18.08.2022/problem7.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._08._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the value");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{y}{y}{y}");
+            Console.WriteLine($"{y} {y}");
+            Console.WriteLine($"{y} {y}");
+            Console.WriteLine($"{y} {y}");
+            Console.WriteLine($"{y}{y}{y}");
+            Console.ReadLine();
+        }
+    }
+}
