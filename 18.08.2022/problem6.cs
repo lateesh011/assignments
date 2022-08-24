34a80afe6b6f0103d997dda02e670fa30f82686b
lateesh011 <111356537+lateesh011@users.noreply.github.com>
2022-08-24 08:33:34 +0530

    Create problem6.cs

---
+++ 18.08.2022/problem6.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._08._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter your age: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine($"You look older than {x}");
+            Console.ReadLine();
+
+        }
+    }
+}
