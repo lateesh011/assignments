071414d4328507388f2607d85b4a59fce574db6c
lateesh011 <111356537+lateesh011@users.noreply.github.com>
2022-08-24 08:30:30 +0530

    Create problem2.cs

---
+++ 18.08.2022/problem2.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._08._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 25;
+            int b = 4;
+            int c = a + b;
+            int d = a - b;
+            int e = a * b;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+    }
+}
