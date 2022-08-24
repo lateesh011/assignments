151495c6966d49315a91a183478da57515e64e96
lateesh011 <111356537+lateesh011@users.noreply.github.com>
2022-08-24 08:59:40 +0530

    Update prblm8.cs

--- 19.08.2022/prblm8.cs
+++ 19.08.2022/prblm8.cs
@@ -10,7 +10,15 @@ namespace _19._08._2022
     {
         static void Main(string[] args)
         {
-        
+         for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
