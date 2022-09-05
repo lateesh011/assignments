71cd178365d264d3ee9b3ac5b44b60e7a08454e6
lateesh011 <111356537+lateesh011@users.noreply.github.com>
2022-09-05 08:09:30 +0530

    Create shellsort.cs

---
+++ 01.09.2022/shellsort.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shellsort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShellSort();
+            Console.Read();
+
+        }
+        static void printAll(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+            static  void ShellSort()
+            {
+                int[] arr = new int[] { 33, 45, 5, 25, 32, 12, 88, 65, 6 };
+                int inner, outer, valueToInsert, interval = 1, i = 0;
+                Console.WriteLine("Before Sort the array=");
+                printAll(arr);
+                Console.WriteLine("----------------------------");
+                while (interval <= ((arr.Length) / 3))
+                {
+                    //  h=h*3+1
+                    interval = interval * 3 + 1;
+                }
+                while (interval > 0)
+                {
+                    for (outer = 0; outer < arr.Length; outer++)
+                    {
+                        valueToInsert = arr[outer];
+                        inner = outer;
+                        while ((inner > interval - 1) && (arr[inner - interval] >= valueToInsert))
+                        {
+                            arr[inner] = arr[inner - interval];
+                            inner = inner - interval;
+                            Console.WriteLine("Item moved= " + arr[inner]);
+                        }
+                        arr[inner] = valueToInsert;
+                        Console.WriteLine("Item inserted at =" + arr[inner]);
+                    }
+                    interval = (interval - 1) / 3;
+                    i += 1;
+                }
+                Console.WriteLine("After Sort the array=");
+                printAll(arr);
+
+            }
+        }
+    }
