74b036067db770148a2a0560fc6c4ad9063675ab
lateesh011 <111356537+lateesh011@users.noreply.github.com>
2022-09-12 08:20:32 +0530

    Create prblm4.cs

---
+++ 09.09.2022/prblm4.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _4_Mutex
+{
+    class MutexDemo
+    {
+        static void Main(string[] args)
+        {
+            
+            using (Mutex mutex = new Mutex(false, "ThreadingDemo"))
+            {
+
+                if (!mutex.WaitOne(5000, false))
+                {
+                    Console.WriteLine("already running....");
+                    Console.ReadLine();
+                    return;
+
+                }
+                else
+                {
+                    Console.WriteLine("app is running");
+                    Console.ReadLine();
+                }
+
+            }
+
+        }
+    }
+}
